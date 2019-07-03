722b15f19c60d97566cccbde298a6651a315c3e7
Gabe Sumner <gabe.sumner@gmail.com>
2019-07-03 07:02:55 -0700

    Changed org life to 3 days for testing.

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,8 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s -n --userprefix admin --userdomain electron.demo
+sfdx shane:org:create -f config/project-scratch-def.json -d 3 -s -n --userprefix admin --userdomain electron.demo
 
 sfdx force:source:push
 sfdx force:user:permset:assign -n electron
 
 sfdx shane:theme:activate -n Electron
 sfdx force:org:open
-sfdx shane:user:password:set -g User -l User -p platformer1
+sfdx shane:user:password:set -g User -l User -p sfdx1234
